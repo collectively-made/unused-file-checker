47bdebbed558b3ad8dff983c68bc9059b85474fc
Hayden Gascoigne <haydengascoigne@gmail.com>
2015-12-29 15:58:30 -0600

    Some initial scaffolding

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
+import scanner from 'course-file-scanner';
+
 /**
  * @param {Type}
  * @return {Type}
  */
-export default function () {
-  return true
+export default function (filePath) {
+  let courseFile = scanner(filePath);
+
+  return {
+    scan: () => {
+      // Initialize response object
+      let returnObject = {
+        unusedFiles: [],
+        errors: []
+      };
+
+      // Return the final response
+      return returnObject;
+    }
+  };
 }
--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
-import test from "tape"
-import unusedFileChecker from "../src"
+import test from 'tape';
+import unusedFileChecker from '../src';
 
-test("unusedFileChecker", (t) => {
-  t.plan(1)
-  t.equal(true, unusedFileChecker(), "return true")
-})
+test('One or more unused files are found', (t) => {
+  t.plan(1);
+
+  let result = unusedFileChecker('test-course.zip').scan();
+  t.equals((result.hasOwnProperty('unusedFiles') && result.unusedFiles.length > 0), true, 'Should return object with "unusedFiles" array with a length > 0.');
+});
